bddd0f9efe8a13e15d992a382f525bfdcc4132d6
Miguel Angel <mikeangelodj@gmail.com>
2018-05-02 09:13:20 -0400

    Middlewares

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
 import Home from '../pages/containers/home';
-// import Playlist from './src/playlist/components/playlist';
-// import data from '../api.json';
-// console.log('Hola mundo!' )
-// import data from './../schemas';
-
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './../reducers';
 import { Map } from 'immutable';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-/*const initialState = {
-  data: {
-    entities: data.entities,
-    categories: data.result.categories,
-  },
-  search: [],
-};*/
+/* const logger = store => next => action => {
+  console.log('====================================OLD STATE');
+  console.log(store.getState().toJS());
+  console.log('====================================');
+  console.log('====================================DISPATCHING');
+  console.log(action);
+  console.log('====================================');
+  let result = next(action);
+
+  console.log('====================================STATE');
+  console.log(store.getState().toJS());
+  console.log('====================================');
+
+  return result;
+}; */
 
 const store = createStore(
   reducer,
   Map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(logger)
+  )
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const homeContainer = document.getElementById('home-container')
-
-// ReactDOM.render(que voy a renderizar, donde lo haré);
-// const holaMundo = <h1>hola Estudiante!</h1>;
 
 hydrate(
   <Provider store={store}>
